b458b573832591be83098bd48f1164cc42371bdd
chonchon0516 <azumi.toyoda@gmail.com>
2023-09-06 09:07:21 +0900

    feat:学習アプリclass化

--- src/public/delete.php
+++ src/public/delete.php
@@ -1,16 +1,10 @@
 <?php
-$dbUserName = 'root';
-$dbPassword = 'password';
-$pdo = new PDO(
-    'mysql:host=mysql; dbname=learningmanagement; charset=utf8',
-    $dbUserName,
-    $dbPassword
-);
+require_once __DIR__ . '/../vendor/autoload.php';
+use App\LearningManager;
 
+$learningManager = new LearningManager();
 $id = filter_input(INPUT_GET, 'id');
-$sql = "DELETE FROM learningnotes where id = $id";
-$statement = $pdo->prepare($sql);
-$statement->execute();
+$learningManager->deleteUser($id);
 
 header('Location: ./index.php');
 exit();
